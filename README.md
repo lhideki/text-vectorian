45a08d3eb3b5798a26e164f698ccf8d1924df6b2
hideki <hideki@inoue-kobo.com>
2019-01-16 16:09:48 +0900

    PyPI登録用にsetup.pyを修正しました。

--- README.md
+++ README.md
@@ -17,22 +17,7 @@ TokenizerやVectorizerの詳細を気にすることなく、任意のテキス�
 ## Usage
 
 ```bash
-pip install numpy
-pip install sentencepiece
-pip install gensim
-pip install pyyaml
-pip install git+https://github.com/lhideki/text-vectorian
-```
-
-### 注意事項
-
-学習済みモデルの取得をgitで行っているためgit lfsがインストールされている必要があります。
-git lfsが未導入の場合はgensimのモデルロードで失敗するため注意してください。
-
-以下はMacOSでのgit-lfsのインストール例です。
-
-```bash
-brew install git-lfs
+pip install text-vectorian
 ```
 
 ## Examples
--- setup.py
+++ setup.py
@@ -3,23 +3,21 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
+modules = ['gensim', 'sentencepiece', 'keras']
 
 setup(
     name='text_vectorian',
-    version='0.1.0',
+    version='0.1.1',
     description='For getting token embedded vectors for NLP.',
     long_description=readme,
     author='Hideki INOUE',
     author_email='hideki@inoue-kobo.com',
     url='https://github.com/lhideki/text-vectorian',
-    license=license,
+    license='MIT',
+    install_requires=modules,
     packages=find_packages(exclude=('tests', 'docs', '.vscode')),
     package_data={
         'text_vectorian':
-            ['config.yml',
-            'models/wikija_sentencepiece.model',
-            'models/wikija_sentencepieced_word2vec.model']
+            ['config.yml']
     }
 )
