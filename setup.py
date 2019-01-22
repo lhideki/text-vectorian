a86870e7813b71140540979bdfbfa0e5b5440ba4
hideki <hideki@inoue-kobo.com>
2019-01-22 14:33:00 +0900

    Char2VecVectorianを実装しました。

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ modules = ['gensim', 'sentencepiece', 'keras']
 
 setup(
     name='text_vectorian',
-    version='0.1.4',
+    version='0.1.5',
     description='For getting token embedded vectors for NLP.',
     long_description=readme,
     long_description_content_type='text/markdown',
